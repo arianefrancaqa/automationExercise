65f3ca71079858044a83a76bdd94e633409ab403
arianefrancaqa <arianefrancaqa@gmail.com>
2024-04-10 09:54:57 -0300

    adding remaining tests

--- cypress/e2e/api/users.cy.js
+++ cypress/e2e/api/users.cy.js
@@ -17,7 +17,59 @@ describe(`Users API Tests`, () => {
             })
             return request.then((response) => {
                 expect(response.status).to.eq(200);
+                cy.putUpdateAccount({
+                    name: `${randomData.firstName} Updated`,
+                    email: randomData.email,
+                    password: randomData.password,
+                    firstName: randomData.firstName,
+                    lastName: randomData.lastName,
+                    companyName: randomData.companyName,
+                    address: randomData.streetAddressUS,
+                    country: 'United States',
+                    zipcode: randomData.zipCodeUS,
+                    state: randomData.stateUS,
+                    city: randomData.cityUS,
+                    phone: randomData.phoneUS
+                }).then(response => {
+                    const responseBody = JSON.parse(response.body);
+                    expect(responseBody).to.have.property('responseCode', 200);
+                    expect(responseBody).to.have.property('message', 'User updated!');
+                });
             });
+
+
+        })
+    });
+
+    it('#11 Should be able to delete user through API', () => {
+        cy.generateRandomData().then((randomData) => {
+            const request = cy.postCreateAccount({
+                name: randomData.firstName,
+                email: randomData.email,
+                password: randomData.password,
+                firstName: randomData.firstName,
+                lastName: randomData.lastName,
+                companyName: randomData.companyName,
+                address: randomData.streetAddressUS,
+                country: 'United States',
+                zipcode: randomData.zipCodeUS,
+                state: randomData.stateUS,
+                city: randomData.cityUS,
+                phone: randomData.phoneUS
+            })
+            return request.then((response) => {
+                expect(response.status).to.eq(200);
+                cy.deleteAccount({
+                    email: randomData.email,
+                    password: randomData.password
+                }).then(response => {
+                    const responseBody = JSON.parse(response.body);
+                    expect(responseBody).to.have.property('responseCode', 200);
+                    expect(responseBody).to.have.property('message', 'Account deleted!');
+                });
+            });
+
+
         })
     });
 
